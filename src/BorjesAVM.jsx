41b5d4a576cde4548d1b929b0a9678c86b20b039
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2015-08-23 00:37:12 +0200

    Editable AVMs

--- src/BorjesAVM.jsx
+++ src/BorjesAVM.jsx
@@ -26,19 +26,36 @@ class BorjesAVM extends React.Component {
         this.props.update(x);
     }
 
+    addF () {
+        var f = React.findDOMNode(this.refs.newF).value;
+        var x = this.props.x;
+        FStruct.set(x, f, Bjs.types.Anything);
+        this.props.update(x);
+    }
+
+    rmF (f) {
+        var x = this.props.x;
+        FStruct.unset(x, f);
+        this.props.update(x);
+    }
+
     render () {
         var x = this.props.x;
+        var opts = this.props.opts;
         var atrs = x.f;
         return (<table className="borjes_fs">
             {x.borjes === 'tfstruct'?<thead><tr><th colSpan="2" onClick={this.toggle.bind(this)}>
-            <BorjesComponent x={x.type} opts={this.props.opts} /></th></tr></thead>:null}
+            <BorjesComponent x={x.type} opts={opts} /></th></tr></thead>:null}
             <tbody className={this.state.show?'borjes_visible':'borjes_hidden'} >
             {atrs.map(f => {
                 return (<tr key={f}>
-                    <td className="borjes_feat">{f}</td>
-                    <td><BorjesComponent update={this.updateF.bind(this, f)} x={FStruct.get(x, f)} opts={this.props.opts}/></td>
+                    <td className="borjes_feat">
+                        {opts.editable?<button onClick={this.rmF.bind(this, f)}>x</button>:null}
+                        {f}</td>
+                    <td><BorjesComponent update={this.updateF.bind(this, f)} x={FStruct.get(x, f)} opts={opts}/></td>
                 </tr>);
             })}
+            {opts.editable?<td><input ref="newF" type="text" /><button onClick={this.addF.bind(this)}>+</button></td>:null}
             </tbody>
         </table>);
     }
